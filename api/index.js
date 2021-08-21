7419818582d38e5621dfd7099bf0445fd5579536
anshumansinha18 <sinhaanshuman18298@gmail.com>
2021-08-22 03:18:29 +0530

    Rest API User CRUD Operations

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv'); //used to import .env file
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ mongoose.connect(process.env.MONGO_URL, {
 
 app.use(express.json());
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 
 
